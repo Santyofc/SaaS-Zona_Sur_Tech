74d7772bb9bc125d53baa23dd66a20049cb7bdbe
Santidevs <Santyofc@users.noreply.github.com>
2026-03-28 19:40:44 -0600

    fix: production optimization (seo, performance, gtm, adsense, nginx)

--- apps/web/src/app/(marketing)/layout.tsx
+++ apps/web/src/app/(marketing)/layout.tsx
@@ -9,12 +9,30 @@ export default function MarketingLayout({
 }) {
   return (
     <>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KGL6G7TG');
+            `}
+          </Script>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGL6G7TG" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+        </>
+      )}
       <Script 
         id="adsense"
         async 
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8338467922774671"
         crossOrigin="anonymous"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
       <Header />
       {children}
